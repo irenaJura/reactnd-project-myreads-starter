b1eda03e50309690f3aeb147d3dafae86b22f30c
Irena <irenaskydive@gmail.com>
2018-07-28 18:45:39 +0200

    query books with booksAPI

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,28 +1,51 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import Book from './Book'
+import * as BooksAPI from './BooksAPI'
 import escapeRegExp from 'escape-string-regexp'
 import sortBy from 'sort-by'
 
 class SearchPage extends Component {
   state = {
-    query: ""
+    query: "",
+    queriedBooks: []
   }
 
-  updateQuery = (query) => {
-    this.setState({ query: query.trim() })
-  }
+  queryBooks = query => {
+    let queriedBooks = [];
+    
+    this.setState({
+      query: query.trim()
+    });
 
-  render() {
-    let showBooks
-    if(this.state.query) {
-      const match = new RegExp(escapeRegExp(this.state.query), "i")
-      showBooks = this.props.books.filter(filtered => match.test(filtered.title))
+    if (query) {
+      let queryResults = [];
+
+      BooksAPI.search(query).then(data => {
+        if (data && data.length) {
+          queryResults = data;
+
+          this.setState({
+            queriedBooks: queryResults
+          });
+        } else {
+          this.setState({
+            queriedBooks: []
+          });
+        }
+      });
+
     } else {
-      showBooks = this.props.books;
+      this.setState({
+        queriedBooks: []
+      });
     }
 
-    showBooks.sort(sortBy("title"))
+  };
+
+
+  render() {
+   
 
     return (
         <div className="search-books">
@@ -37,13 +60,13 @@ class SearchPage extends Component {
               However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
               you don't find a specific author or title. Every search is limited by search terms.
             */}
-            <input type="text" placeholder="Search by title or author" onChange={(event) => this.updateQuery(event.target.value)} />
+            <input type="text" placeholder="Search by title or author" onChange={(event) => this.queryBooks(event.target.value)} />
 
           </div>
         </div>
         <div className="search-books-results">
           <ol className="books-grid">
-            <Book filtered={showBooks} changeShelf={this.props.changeShelf} />
+            <Book filtered={this.state.queriedBooks} changeShelf={this.props.changeShelf} />
           </ol>
         </div>
       </div>
